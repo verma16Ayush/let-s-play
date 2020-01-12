b1260277433788e4908afda1a71a710389c0bc47
verma16ayush <ayush.bravo16@gmail.com>
2020-01-12 09:52:56 +0530

    comment2.0

--- playGame.py
+++ playGame.py
@@ -26,21 +26,23 @@ def take_input():
 
 
 def check_win(board_input):
-    if 'XXX' in ch.join(board_input):
+    if 'XXX' in ch.join(board_input) and (ch.join(board_input).index('XXX')==1 or ch.join(board_input).index('XXX')==4 or ch.join(board_input).index('XXX')==7):
         return True, 'X'
     elif 'XXX' in ch.join(board_input[1::4]):
         return True, 'X'
-    elif 'XXX' in ch.join(board_input[7::-2]):
+    elif 'XXX' in ch.join(board_input[7:2:-2]):
         return True, 'X'
-    elif 'OOO' in ch.join(board_input):
+    elif 'OOO' in ch.join(board_input)and (ch.join(board_input).index('OOO')==4 or ch.join(board_input).index('OOO')==1 or ch.join(board_input).index('OOO')==7):
         return True, 'O'
     elif 'OOO' in ch.join(board_input[1::4]):
         return True, 'O'
-    elif 'OOO' in ch.join(board_input[7::-2]):
+    elif 'OOO' in ch.join(board_input[7:2:-2]):
         return True, 'O'
-    elif 'OOO' in ch.join(board_input[1::3]) or 'OOO' in ch.join(board_input[2::3]) or 'OOO' in ch.join(board_input[3::3]):
+    elif 'OOO' in ch.join(board_input[1::3]) or 'OOO' in ch.join(board_input[2::3]) or 'OOO' in ch.join(
+            board_input[3::3]):
         return True, 'O'
-    elif 'XXX' in ch.join(board_input[1::3]) or 'XXX' in ch.join(board_input[2::3]) or 'XXX' in ch.join(board_input[3::3]):
+    elif 'XXX' in ch.join(board_input[1::3]) or 'XXX' in ch.join(board_input[2::3]) or 'XXX' in ch.join(
+            board_input[3::3]):
         return True, 'X'
     else:
         return False, 'y'
@@ -51,20 +53,21 @@ def driver():
     play = True
     while play:
         board_input = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        available_inputs = ['#', 1, 2, 3, 4, 5, 6, 7, 8, 9]
         p1, p2 = take_input()
-        play_order = 1
+        display_board(board_input)
         for i in range(1, 11, 1):
-            display_board(board_input)
-            if play_order == 1:
-                if i % 2 == 0:
-                    board_input[int(input())] = p2
-                else:
-                    board_input[int(input())] = p1
-            else:
-                if i % 2 == 0:
-                    board_input[int(input())] = p1
-                else:
-                    board_input[int(input())] = p2
+            x = int(input())
+            if x not in available_inputs:
+                print('invalid input. place already occupied. try again.')
+                i -= 1
+                continue
+            if i % 2 == 0 and x in available_inputs:
+                board_input[x] = p2
+                available_inputs.pop(available_inputs.index(x))
+            elif i % 2 != 0 and x in available_inputs:
+                board_input[x] = p1
+                available_inputs.pop(available_inputs.index(x))
             win, player = check_win(board_input)
             display_board(board_input)
             if win and player == p1:
@@ -73,7 +76,7 @@ def driver():
             elif win and player == p2:
                 print('player 2 has won')
                 break
-            elif not win and i == 10:
+            elif len(available_inputs)==1:
                 print('Game tie.')
                 break
         play = (bool(int(input('Do you want to play again?\n 1=yes\n 0=no: '))))
